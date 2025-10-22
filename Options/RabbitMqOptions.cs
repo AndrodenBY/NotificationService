c5b569437a4765b0797142a7ded4619528a0c284
AndrodenBY <56772046+AndrodenBY@users.noreply.github.com>
2025-10-22 16:03:25 +0200

    feat(core): add options for containerization

--- Options/RabbitMqOptions.cs
+++ Options/RabbitMqOptions.cs
@@ -5,7 +5,7 @@ public class RabbitMqOptions
     public const string SectionName = "RabbitMQ";
 
     public string HostName { get; set; } = default!;
-    public string VirtualHost { get; set; } = default!;
+    public string VirtualHostName { get; set; } = default!;
     public string UserName { get; set; } = default!;
     public string Password { get; set; } = default!;
 }
