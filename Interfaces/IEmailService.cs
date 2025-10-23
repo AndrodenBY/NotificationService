89f2dda399f04b068ee4adde7f00cf4a923da59b
AndrodenBY <56772046+AndrodenBY@users.noreply.github.com>
2025-10-23 17:36:22 +0200

    feat(core): implement EmailService

---
+++ Interfaces/IEmailService.cs
@@ -0,0 +1,9 @@
+using SubsTracker.Messaging.Contracts;
+
+namespace NotificationService.Interfaces;
+
+public interface IEmailService
+{
+    Task Send<TEvent>(string recipientEmail, string subject, string templatePath, TEvent eventModel,
+        CancellationToken cancellationToken) where TEvent : BaseEvent;
+}
